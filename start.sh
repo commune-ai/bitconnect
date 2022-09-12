70ff9593a7770ad8469270fed52725d12152e65f
Vivona <vivonasg@gmail.com>
2022-09-12 14:20:32 -0400

    more refactoring

---
+++ start.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 Ocean Protocol contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: ./start_ocean.sh
+#
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+
+IP="localhost"
+optspec=":-:"
+set -e
+
+# Patch $DIR if spaces
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="${DIR/ /\\ }"
+
+# Default versions of Aquarius, Provider
+
+export PROJECT_NAME="wholetensor"
+export FORCEPULL="false"
+
+# Export User UID and GID
+export LOCAL_USER_ID=$(id -u)
+export LOCAL_GROUP_ID=$(id -g)
+
+
+# Add aquarius to /etc/hosts
+# Workaround mainly for macOS
+
+
+# colors
+COLOR_R="\033[0;31m"    # red
+COLOR_G="\033[0;32m"    # green
+COLOR_Y="\033[0;33m"    # yellow
+COLOR_B="\033[0;34m"    # blue
+COLOR_M="\033[0;35m"    # magenta
+COLOR_C="\033[0;36m"    # cyan
+
+# reset
+COLOR_RESET="\033[00m"
+
+
+
+function check_if_owned_by_root {
+    if [ -d "$OCEAN_HOME" ]; then
+        uid=$(ls -nd "$OCEAN_HOME" | awk '{print $3;}')
+        if [ "$uid" = "0" ]; then
+            printf $COLOR_R"WARN: $OCEAN_HOME is owned by root\n"$COLOR_RESET >&2
+        else
+            uid=$(ls -nd "$OCEAN_ARTIFACTS_FOLDER" | awk '{print $3;}')
+            if [ "$uid" = "0" ]; then
+                printf $COLOR_R"WARN: $OCEAN_ARTIFACTS_FOLDER is owned by root\n"$COLOR_RESET >&2
+            fi
+        fi
+    fi
+}
+
+
+function check_max_map_count {
+  vm_max_map_count=$(docker run --rm busybox sysctl -q vm.max_map_count)
+  vm_max_map_count=${vm_max_map_count##* }
+  vm_max_map_count=262144
+  if [ $vm_max_map_count -lt 262144 ]; then
+    printf $COLOR_R'vm.max_map_count current kernel value ($vm_max_map_count) is too low for Elasticsearch\n'$COLOR_RESET
+    printf $COLOR_R'You must update vm.max_map_count to at least 262144\n'$COLOR_RESET
+    printf $COLOR_R'Please refer to https://www.elastic.co/guide/en/elasticsearch/reference/6.6/vm-max-map-count.html\n'$COLOR_RESET
+    exit 1
+  fi
+}
+
+function clean_local_contracts {
+    rm -f "${OCEAN_ARTIFACTS_FOLDER}/ready"
+    rm -f "${OCEAN_ARTIFACTS_FOLDER}/*.json"
+}
+
+check_if_owned_by_root
+
+
+COMPOSE_FILES=""
+COMPOSE_FILES+=" -f ${DIR}/backend/docker-compose-local.yml"
+COMPOSE_FILES+=" -f ${DIR}/subtensor/docker-compose-local.yaml"
+
+
+FORCEPULL="true"
+
+
+while :; do
+    case $1 in
+        #################################################
+        # Cleaning switches
+        #################################################
+        --pull)
+        FORCEPULL="true"
+        ;;
+        --purge)
+            printf "$COMPOSE_FILES"
+            printf $COLOR_R'Doing a deep clean ...\n\n'$COLOR_RESET
+            eval docker-compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
+            docker network rm ${PROJECT_NAME}_default || true;
+            docker network rm ${PROJECT_NAME}_backend || true;
+            shift
+            break
+            ;;
+
+        --down)
+            printf $COLOR_R'Doing a deep clean ...\n\n'$COLOR_RESET
+            eval docker network rm ${PROJECT_NAME}_default || true;
+            eval docker-compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
+        ;;
+
+        
+        --restart)
+            printf $COLOR_R'Doing a deep clean ...\n\n'$COLOR_RESET
+            eval docker-compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
+            docker network rm ${PROJECT_NAME}_default || true;
+
+            [ ${FORCEPULL} = "true" ] && eval docker-compose  --project-name=$PROJECT_NAME "$COMPOSE_FILES" pull
+            [ ${FORCEPULL} = "true" ] && eval docker-compose  --project-name=$PROJECT_NAME "$COMPOSE_FILES" build
+            
+            eval docker-compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME "$COMPOSE_FILES" up --remove-orphans -d
+           
+           
+            break
+            ;;
+
+        --) # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf $COLOR_R'WARN: Unknown option (ignored): %s\n'$COLOR_RESET "$1" >&2
+            break
+            ;;
+        *)
+            eval docker-compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME "$COMPOSE_FILES" build
+            eval docker-compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME "$COMPOSE_FILES" pull
+            eval docker-compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME  "$COMPOSE_FILES" up --remove-orphans -d
+            break
+    esac
+    shift
+done
+
+
+
